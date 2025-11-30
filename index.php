b31d9f588dc1917cb2c489751d0bf1e2a4163015
khushbakht-khan <f223630@cfd.nu.edu.pk>
2025-11-30 15:16:50 +0500

    QP-25 Design navigation bar UI

--- index.php
+++ index.php
@@ -11,13 +11,7 @@ unset($_SESSION['errors'], $_SESSION['old']);
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>QuickPOS - The Last POS System You'll Ever Need</title>
-
-<!-- Font Awesome -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
-<!-- AOS Animation Library -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" />
+<title>QuickPOS - The Last POS System You'll Ever Need for your business</title>
 
 <style>
 /* Root Colors */
